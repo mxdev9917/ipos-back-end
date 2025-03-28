1208b17e9e51bb2b64b174f12a33ca4049204ac2
eh.dev <131841645+Mx9917@users.noreply.github.com>
2025-03-29 00:41:16 +0700

    update

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -4,19 +4,26 @@ const encrypt = require('./encrypt');
 exports.verifyToken = async (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return errors.mapError(404, 'Token undefined', next);  // No Authorization header
+        return errors.mapError(404, 'Token undefined', next);
     }
-    // Check if the Authorization header is in the correct "Bearer <token>" format
-    let token = authHeader.split(' ')[1];  // Assuming the format is "Bearer <token>"
+
+    // Extract the token from the "Bearer <token>" format
+    const token = authHeader.split(' ')[1];
     if (!token) {
         return errors.mapError(401, 'Token missing', next);
     }
+
     try {
         const tokenValid = await encrypt.verifyToken(token);
+        req.user = tokenValid; // Save user info from token if needed
+        next(); // Proceed to the next middleware
     } catch (error) {
         console.log(error.message);
-        errors.mapError(401, 'Token invalid', next);
-    }
-    next();
+        
+        if (error.name === "TokenExpiredError") {
+            return errors.mapError(401, 'Token expired, please log in again', next);
+        }
 
-}
+        return errors.mapError(401, 'Token invalid', next);
+    }
+};
