592388231ece6f397fac25028c6d7469a2d0b5dc
eh.dev <131841645+Mx9917@users.noreply.github.com>
2025-04-03 17:18:09 +0700

    update file database

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const encrypt = require('../utils/encrypt');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const errors = require('../utils/errors')
 dotenv.config({ path: './config.env' });
 
 const connection = mysql.createConnection({
@@ -12,10 +13,10 @@ const connection = mysql.createConnection({
     connectTimeout: process.env.DB_CONNECTTIMEOUT, // 10 seconds
 });
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_USER:', process.env.DB_USER);
+// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+// console.log('DB_DATABASE:', process.env.DB_DATABASE);
 
 // Connect to the database
 connection.connect((error) => {
@@ -32,17 +33,19 @@ connection.connect((error) => {
         password: process.env.PASSWORD,
         status: 'active',
         img: '',
-        
+
     };
-    const checkQuery = 'SELECT COUNT(*) AS count FROM User_admins WHERE user_admin_email = ?';
-    connection.query(checkQuery, [userData.email], async (error, results) => {
-        if (error) {
-            console.error('Error executing select query:', error.message);
-            return;
-        }
-        const count = results[0].count;
-        if (count === 0) {
-            const insertQuery = `
+
+    try {
+        const checkQuery = 'SELECT COUNT(*) AS count FROM User_admins WHERE user_admin_email = ?';
+        connection.query(checkQuery, [userData.email], async (error, results) => {
+            if (error) {
+                console.error('Error executing select query:', error.message);
+                return;
+            }
+            const count = results[0].count;
+            if (count === 0) {
+                const insertQuery = `
                 INSERT INTO User_admins (
                     user_admin_name, 
                     user_admin_email, 
@@ -54,23 +57,31 @@ connection.connect((error) => {
                 ) 
                 VALUES (?, ?, ?, ?, ?, ?, ?);
             `;
-            connection.query(insertQuery, [
-                userData.name, 
-                userData.email, 
-                userData.phone, 
-                userData.role, 
-               await encrypt.hashPassword(userData.password),
-                userData.status, 
-                userData.img
-            ], (error, results) => {
-                if (error) {
-                    console.error('Error executing insert query:', error.message);
-                    return;
-                }
-                console.log('User successfully inserted:', results.insertId);
-            });
-        } 
-    });
+                connection.query(insertQuery, [
+                    userData.name,
+                    userData.email,
+                    userData.phone,
+                    userData.role,
+                    await encrypt.hashPassword(userData.password),
+                    userData.status,
+                    userData.img
+                ], (error, results) => {
+                    if (error) {
+                        console.error('Error executing insert query:', error.message);
+                        return;
+                    }
+                    console.log('User successfully inserted:', results.insertId);
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error.message);
+        errors.mapError(500, "Internal server error", next);
+    }
+
+
+
+
 
 });
 
